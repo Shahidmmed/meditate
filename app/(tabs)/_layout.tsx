a8f9af03b3dbebba771175ab361acabf7c67cf7f
Shahid <shahidmmed@gmail.com>
2024-10-09 15:24:39 +0000

    init commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,43 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { MaterialCommunityIcons, Entypo } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="nature-meditate"
+        options={{
+          title: "Meditate",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="flower-tulip"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="affirmations"
+        options={{
+          title: "Affirmations",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="open-book" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
